f1cb9deb8540a5827bc9c82ceb4f32200a93ea2c
Filip <f@filip.world>
2023-12-15 14:26:21 -0500

    JBOwnable

--- src/JBOwnable.sol
+++ src/JBOwnable.sol
@@ -2,20 +2,21 @@
 // Juicebox variation on OpenZeppelin Ownable
 pragma solidity ^0.8.0;
 
-import {JBOwnableOverrides, IJBProjects, IJBPermissions} from "./JBOwnableOverrides.sol";
+import {JBOwnableOverrides} from "./JBOwnableOverrides.sol";
+import {IJBProjects} from "lib/juice-contracts-v4/src/interfaces/IJBProjects.sol";
+import {IJBPermissions} from "lib/juice-contracts-v4/src/interfaces/IJBPermissions.sol";
 
 contract JBOwnable is JBOwnableOverrides {
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     /**
-     * @param projects the JBProjects to use to get the owner of the project
-     *   @param permissions the permissions to use for the permissions
+     * @param projects The `IJBProjects` to use for tracking project ownership.
+     * @param permissions The `IJBPermissions` to use for managing permissions.
      */
     constructor(IJBProjects projects, IJBPermissions permissions) JBOwnableOverrides(projects, permissions) {}
 
-    /**
-     * @dev Throws if called by an account that is not the owner and does not have permission to act as the owner
-     */
+
+    /// @notice Reverts if called by an address that is not the owner and does not have permission from the owner.
     modifier onlyOwner() virtual {
         _checkOwner();
         _;
